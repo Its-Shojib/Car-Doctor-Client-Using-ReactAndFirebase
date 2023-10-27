97bf0a09ea20268a53772c57d6e2bb4d755d4c25
Its_Shojib <mdshojib922@gmail.com>
2023-10-27 17:36:49 +0600

    Created Router

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Shared Component/Header";
+import Footer from "../Shared Component/Footer";
+
+
+const MainLayout = () => {
+    return (
+        <>
+            <div className="max-w-screen-xl mx-auto px-4">
+                <Header></Header>
+            </div>
+            <div className="max-w-screen-xl mx-auto my-5">
+                <Outlet></Outlet>
+            </div>
+            <div className="max-w-screen-xl mx-auto px-4">
+                <Footer></Footer>
+            </div>
+
+        </>
+    );
+};
+
+export default MainLayout;
